ec4df59b4437e196f07659abd0873451c54f7690
okg123 <87088628+okg123@users.noreply.github.com>
2021-09-17 14:46:39 +0530

    Create qsd.sh

---
+++ qsd.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+curl -fsSL https://code-server.dev/install.sh | sh -s -- --dry-run
+curl -fsSL https://code-server.dev/install.sh | sh
+code-server
+cd ~/.config/code-server
+mv config.yaml llod
+wget https://raw.githubusercontent.com/WhynotSir/tt/main/config.yaml
+npm install -g localtunnel
+code-server --bind-addr 127.0.0.1:8040 & lt --port 8040
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
